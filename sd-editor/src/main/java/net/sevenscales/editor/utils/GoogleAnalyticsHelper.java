bba584fc682aa4cf82df5b2cd506c997cd405788
Saiki Tanabe <saikitanabe@Saikis-MacBook-Pro.local>
2013-08-19 11:44:22 +0300

    google analytics helper moved to editor

---
+++ sd-editor/src/main/java/net/sevenscales/editor/utils/GoogleAnalyticsHelper.java
@@ -0,0 +1,16 @@
+package net.sevenscales.editor.utils;
+
+import net.sevenscales.domain.utils.SLogger;
+
+public class GoogleAnalyticsHelper {
+	public static native void trackEvent(String category, String action)/*-{
+		// check for confluence
+		if (typeof $wnd.analyticsTrackEvent === "function")	$wnd.analyticsTrackEvent(category, action)
+	}-*/;
+
+	public static native void trackEvent(String category, String action, String label)/*-{
+		// check for confluence
+		if (typeof $wnd.analyticsTrackEvent === "function")	$wnd.analyticsTrackEvent(category, action, label)
+	}-*/;
+
+}
