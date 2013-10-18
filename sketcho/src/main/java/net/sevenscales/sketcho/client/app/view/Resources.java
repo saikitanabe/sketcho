f908b30f14c8c4c8aa304884c81988d28ced3890
Saiki Tanabe <saikitanabe@Saikis-MacBook-Pro.local>
2013-10-18 08:29:43 +0300

    removed obsolete projects from this repository, e.g. old sketcho app

--- sketcho/src/main/java/net/sevenscales/sketcho/client/app/view/Resources.java
+++
@@ -1,9 +0,0 @@
-package net.sevenscales.sketcho.client.app.view;
-
-import com.google.gwt.resources.client.ClientBundle;
-import com.google.gwt.resources.client.ImageResource;
-
-public interface Resources extends ClientBundle {
-  @Source("add.png")
-  ImageResource addSketch();
-}
