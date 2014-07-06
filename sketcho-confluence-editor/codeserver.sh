97b97bc2c60921d4b53ea7d1aa76147fdd632abb
Saiki Tanabe <saiki.tanabe@7scales.net>
2014-07-06 14:04:32 +0300

    gwt 2.6.1

--- sketcho-confluence-editor/codeserver.sh
+++ sketcho-confluence-editor/codeserver.sh
@@ -1,8 +1,9 @@
-GWT=/Users/saikitanabe/tools/gwt-2.5.1
+GWT=/Users/saikitanabe/tools/gwt-2.6.1
 SKETCHO=/Users/saikitanabe/Documents/sources/sketcho
 WORK_DIR=/Users/saikitanabe/sketchboard-confluence-gwt
 RESOURCES=($SKETCHO/sd-domain/src/main/resources:$SKETCHO/hibernate4gwt-domain/src/main/resources:$SKETCHO/sd-app-frame/src/main/resources:$SKETCHO/sd-editor/src/main/resources)
 SOURCES=($SKETCHO/sd-domain/src/main/java:$SKETCHO/hibernate4gwt-domain/src/main/java:$SKETCHO/sd-app-frame/src/main/java:$SKETCHO/sd-editor/src/main/java)
+MEM="-Xmx1024M -XX:MaxPermSize=256M"
 
 #echo "RESOURCES: ${RESOURCES[*]}"
 #echo "SOURCES: ${SOURCES[*]}"
@@ -12,11 +13,10 @@ SOURCES=($SKETCHO/sd-domain/src/main/java:$SKETCHO/hibernate4gwt-domain/src/main
 #/Users/saikitanabe/Documents/sources/sketcho/sketcho-confluence-editor/src/main/java
 #net.sevenscales.sketchoconfluenceapp.Sketcho_confluence_app"
 
-PARAMS="-Xmx1024M -cp $GWT/gwt-codeserver.jar:$GWT/gwt-dev.jar:$GWT/gwt-user.jar:${RESOURCES[*]}:${SOURCES[*]}
-com.google.gwt.dev.codeserver.CodeServer -workDir $WORK_DIR -src
+PARAMS="$MEM -cp $GWT/gwt-codeserver.jar:$GWT/gwt-dev.jar:$GWT/gwt-user.jar:${RESOURCES[*]}:${SOURCES[*]}
+com.google.gwt.dev.codeserver.CodeServer $BIND_ADDRESS -port 8081 -workDir $WORK_DIR -src
 /Users/saikitanabe/Documents/sources/sketcho/sketcho-confluence-editor/src/main/java
 net.sevenscales.sketchoconfluenceapp.Sketcho_confluence_app"
-
 
 echo $PARAMS
 
