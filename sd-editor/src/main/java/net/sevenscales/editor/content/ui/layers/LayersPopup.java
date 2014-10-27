74d04a41b8ff7a96f6ab7bd1fdcfecb9f78fe31a
Saiki Tanabe <saiki.tanabe@7scales.net>
2014-10-27 09:48:10 +0200

    Show hide context menu items
    - color
    - top - bottom
    - link

--- sd-editor/src/main/java/net/sevenscales/editor/content/ui/layers/LayersPopup.java
+++ sd-editor/src/main/java/net/sevenscales/editor/content/ui/layers/LayersPopup.java
@@ -1,6 +1,7 @@
 package net.sevenscales.editor.content.ui.layers;
 
 import com.google.gwt.user.client.ui.PopupPanel;
+import com.google.gwt.dom.client.Element;
 
 import net.sevenscales.editor.api.ISurfaceHandler;
 
@@ -8,9 +9,10 @@ import net.sevenscales.editor.api.ISurfaceHandler;
 public class LayersPopup extends PopupPanel {
 	private LayersMenu layersMenu;
 
-	public LayersPopup(ISurfaceHandler surface) {
+	public LayersPopup(ISurfaceHandler surface, Element launcher) {
 		setStyleName("text-size-popup");
 		setAutoHideEnabled(true);
+		addAutoHidePartner(launcher);
 		layersMenu = new LayersMenu(surface, this);
 		setWidget(layersMenu);
 	}
