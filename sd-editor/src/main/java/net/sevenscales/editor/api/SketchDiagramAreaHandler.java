467fedc748d2a7be52b14ae7bc8a403b54556a0d
Saiki Tanabe <saiki.tanabe@7scales.net>
2015-01-15 14:24:23 +0200

    arrow also auto centred

--- sd-editor/src/main/java/net/sevenscales/editor/api/SketchDiagramAreaHandler.java
+++ sd-editor/src/main/java/net/sevenscales/editor/api/SketchDiagramAreaHandler.java
@@ -133,7 +133,7 @@ public class SketchDiagramAreaHandler implements MouseDiagramHandler {
 
       di.addShapeProperty(ShapeProperty.CLOSEST_PATH);
       // Automatically center connection at first time.
-      // di.addShapeProperty(ShapeProperty.CENTERED_PATH);
+      di.addShapeProperty(ShapeProperty.CENTERED_PATH);
       // Default arrow type is curved ends configuration
 
       this.createdRelationship = new Relationship2(surface, rshape, defaultRelationship, Theme.createDefaultBackgroundColor(), Theme.createDefaultBorderColor(), Theme.createDefaultTextColor(), true, di);
