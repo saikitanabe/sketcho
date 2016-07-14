c81623ff8aa7d1981a6143367799c7dd4896f942
Saiki Tanabe <saiki.tanabe@7scales.net>
2016-07-14 17:47:12 +0300

    Onboarding demo started using JSON items

--- sd-editor/src/main/java/net/sevenscales/editor/api/BoardDimensions.java
+++ sd-editor/src/main/java/net/sevenscales/editor/api/BoardDimensions.java
@@ -3,7 +3,12 @@ package net.sevenscales.editor.api;
 import java.util.List;
 
 import net.sevenscales.editor.diagram.Diagram;
+import net.sevenscales.editor.diagram.shape.Info;
 import net.sevenscales.editor.uicomponents.CircleElement;
+import net.sevenscales.domain.IDiagramItemRO;
+import net.sevenscales.editor.content.utils.DuplicateHelpers;
+import net.sevenscales.editor.content.utils.ShapeParser;
+
 
 public class BoardDimensions {
 	private int leftmost = Integer.MAX_VALUE;
@@ -96,5 +101,50 @@ public class BoardDimensions {
 		int bottom = BoardDimensions.getBottommost(); 
 		return bottom - top;
 	}
+
+	public static int itemsWidth(List<IDiagramItemRO> items) {
+		int left = Integer.MAX_VALUE;
+		int top = Integer.MAX_VALUE;
+		int right = Integer.MIN_VALUE;
+
+		for (IDiagramItemRO di : items) {
+			if (DuplicateHelpers.allowedToPasteType(di)) {
+				Info shape = ShapeParser.parse(di, 0, 0);
+				// Info shape = di.parseShape();
+				int l = shape.getLeft();
+				int t = shape.getTop();
+				int r = l + shape.getWidth();
+
+				left = l < left ? l : left;
+				top = t < top ? t : top;
+				right = r > right ? r : right;
+			}
+		}
+
+		return right - left;
+	}
+
+	public static int itemsHeight(List<IDiagramItemRO> items) {
+		int left = Integer.MAX_VALUE;
+		int top = Integer.MAX_VALUE;
+		int bottom = Integer.MIN_VALUE;
+
+		for (IDiagramItemRO di : items) {
+			if (DuplicateHelpers.allowedToPasteType(di)) {
+				Info shape = ShapeParser.parse(di, 0, 0);
+				// Info shape = di.parseShape();
+				int l = shape.getLeft();
+				int t = shape.getTop();
+				int b = t + shape.getHeight();
+
+				left = l < left ? l : left;
+				top = t < top ? t : top;
+				bottom = b > bottom ? b : bottom;
+			}
+		}
+
+		return bottom - top;
+	}
+
 
 }
