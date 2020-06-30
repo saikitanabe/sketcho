6836a57e445dec09158bcaf1338222438a5eb8bb
Saiki Tanabe <saiki.tanabe@7scales.net>
2020-06-30 16:44:34 +0300

    Add GlobalState.notifySelected

--- sd-editor/src/main/java/net/sevenscales/editor/diagram/GlobalState.java
+++ sd-editor/src/main/java/net/sevenscales/editor/diagram/GlobalState.java
@@ -1,5 +1,7 @@
 package net.sevenscales.editor.diagram;
 
+import com.google.gwt.core.client.JsArrayString;
+
 public class GlobalState {
 
 	public static native void slideCreated()/*-{
@@ -18,6 +20,15 @@ public class GlobalState {
 
 	public static native void notifySaveStatusChanged()/*-{
     $wnd.globalStreams.boardSaveStatusChangedStream.push()
-	}-*/;
+  }-*/;
+  
+  public static native void notifySelected(JsArrayString items)/*-{
+    if (typeof $wnd.globalStreams.selectionStream !== 'undefined') {
+      $wnd.globalStreams.selectionStream.push({
+        operation: 'select',
+        items: items,
+      })
+    }
+  }-*/;
 
 }
