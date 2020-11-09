31b588131cef794e84890d0b65be44c8a80db297
Saiki Tanabe <saiki.tanabe@7scales.net>
2020-11-09 10:19:48 +0200

    Fix freehand white background on export

--- sd-editor/src/main/java/net/sevenscales/editor/uicomponents/uml/GenericFreehandElement.java
+++ sd-editor/src/main/java/net/sevenscales/editor/uicomponents/uml/GenericFreehandElement.java
@@ -5,7 +5,7 @@ import net.sevenscales.editor.diagram.shape.GenericShape;
 import net.sevenscales.editor.gfx.domain.Color;
 import net.sevenscales.domain.IDiagramItemRO;
 import net.sevenscales.editor.content.ui.ContextMenuItem;
-
+import net.sevenscales.editor.api.impl.Theme.ElementColorScheme;
 
 public class GenericFreehandElement extends GenericElement {
 	public GenericFreehandElement(ISurfaceHandler surface, GenericShape newShape, String text, Color backgroundColor, Color borderColor, Color textColor, boolean editable, IDiagramItemRO item) {
@@ -23,5 +23,12 @@ public class GenericFreehandElement extends GenericElement {
 		return super.supportedMenuItems() | ContextMenuItem.LINE_WEIGHT.getValue();
   }
 
+  @Override
+  public boolean usesSchemeDefaultBackgroundColor(
+    ElementColorScheme colorScheme
+  ) {
+    // to fix white backround problem on export
+    return false;
+  }  
 
 }
