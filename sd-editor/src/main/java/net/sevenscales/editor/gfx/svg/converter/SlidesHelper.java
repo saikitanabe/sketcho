a11b32aafe4a5b77833882e48ef75b2082e8ca42
Saiki Tanabe <saiki.tanabe@7scales.net>
2021-05-11 16:32:04 +0300

    Support rotate on slides
    Copies slides using custom react api.

--- sd-editor/src/main/java/net/sevenscales/editor/gfx/svg/converter/SlidesHelper.java
+++ sd-editor/src/main/java/net/sevenscales/editor/gfx/svg/converter/SlidesHelper.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import com.google.gwt.core.client.JsArray;
 import com.google.gwt.core.client.JavaScriptObject;
 
+import net.sevenscales.domain.IDiagramItemRO;
 import net.sevenscales.domain.js.JsPresentation;
 import net.sevenscales.domain.js.JsSlide;
 import net.sevenscales.domain.js.JsSlideData;
@@ -15,6 +16,8 @@ import net.sevenscales.domain.ElementType;
 import net.sevenscales.editor.gfx.svg.converter.SvgConverter;
 import net.sevenscales.domain.api.IDiagramItem;
 import net.sevenscales.editor.api.BoardDimensions;
+import net.sevenscales.editor.api.ot.BoardDocumentHelpers;
+import net.sevenscales.domain.json.JsonExtraction;
 
 public class SlidesHelper {
 
@@ -25,13 +28,24 @@ public class SlidesHelper {
 		List<Diagram> slides = _getSlides(surface);
 
 		SvgBase.setThemeBlack(blackBackground);
-		SvgConverter sc = new SvgConverter(false);
+		// SvgConverter sc = new SvgConverter(false);
 
 		for (Diagram slide : slides) {
-			Diagram[] diagrams = getDiagramsOnArea(surface, slide.getLeft(), slide.getTop(), slide.getWidth(), slide.getHeight());
+			List<Diagram> diagrams = getDiagramListOnArea(surface, slide.getLeft(), slide.getTop(), slide.getWidth(), slide.getHeight());
+			// Diagram[] diagrams = getDiagramsOnArea(surface, slide.getLeft(), slide.getTop(), slide.getWidth(), slide.getHeight());
+
+
 			// SvgData svgdata = sc.diagramsToSvg(surface, diagrams, false, false);
 
-			String svg = sc.diagramsToSvg(surface, diagrams, false, true, -1 * slide.getLeft(), -1 * slide.getTop());
+			// String svg = sc.diagramsToSvg(surface, diagrams, false, true, -1 * slide.getLeft(), -1 * slide.getTop());
+
+      List<? extends IDiagramItemRO> items = BoardDocumentHelpers.diagramsToItems(diagrams);
+
+			String svg = reactDiagramsToSvg(
+        JsonExtraction.decompose(items).getJavaScriptObject(),
+        -1 * slide.getLeft(),
+        -1 * slide.getTop()
+      );
 
 			JsSlideData data = slide.getDiagramItem().getData().cast();
 			jsSlides.push(JsSlide.newSlide(slide.getDiagramItem().getClientId(), data, slide.getLeft(), slide.getTop(), slide.getWidth(), slide.getHeight(), formatSvg(slideWidth, slideHeight, slide.getWidth(), slide.getHeight(), svg)));
@@ -46,6 +60,14 @@ public class SlidesHelper {
 
 		return JsPresentation.newPresentation(width, height, jsSlides);
 	}
+
+	private static native String reactDiagramsToSvg(
+    JavaScriptObject items,
+    int zeroLeft,
+    int zeroTop
+	)/*-{
+		return $wnd.reactDiagramsToSvg(items, zeroLeft, zeroTop);
+	}-*/;
 
 	private static String formatSvg(int slideWidth, int slideHeight, int width, int height, String svg) {
 		if (slideWidth == 0) {
@@ -75,7 +97,7 @@ public class SlidesHelper {
 		return result;
 	}
 
-	private static Diagram[] getDiagramsOnArea(ISurfaceHandler surface, int x, int y, int width, int height) {
+	private static List<Diagram> getDiagramListOnArea(ISurfaceHandler surface, int x, int y, int width, int height) {
 		List<Diagram> result = new ArrayList<Diagram>();
 		for (Diagram d : surface.getDiagrams()) {
 			IDiagramItem di = d.getDiagramItem();
@@ -83,6 +105,12 @@ public class SlidesHelper {
 				result.add(d);
 			}
 		}
+
+		return result;
+	}
+
+	private static Diagram[] getDiagramsOnArea(ISurfaceHandler surface, int x, int y, int width, int height) {
+		List<Diagram> result = getDiagramListOnArea(surface, x, y, width, height);
 
 		Diagram[] r = new Diagram[result.size()];
 		result.toArray(r);
