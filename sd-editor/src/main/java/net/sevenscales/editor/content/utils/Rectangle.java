7a54dac318092e03f1a99a49a97853e4ba43f11d
Saiki Tanabe <saikitanabe@users.noreply.github.com>
2024-04-29 19:39:10 +0300

    Add Rectangle - probably not used at the moment

---
+++ sd-editor/src/main/java/net/sevenscales/editor/content/utils/Rectangle.java
@@ -0,0 +1,15 @@
+package net.sevenscales.editor.content.utils;
+
+public class Rectangle {
+  int x;
+  int y;
+  int width;
+  int height;
+
+  Rectangle(int x, int y, int width, int height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+  }
+}
